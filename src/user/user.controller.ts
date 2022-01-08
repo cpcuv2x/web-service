ddfcd963e1ba1174254247f8b875a2b918c606f4
KKCORR <nattanon.hu@gmail.com>
2022-01-08 18:27:16 +0700

    add comment

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,6 +5,7 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private userService: UserService) {}
 
+  // FIXME: remove this route in production
   @Get()
   getAllUsers() {
     return this.userService.findAll();
