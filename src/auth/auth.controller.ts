df8afe74fad973f148d1d79d81edd5fbe0eafbfc
KKCORR <nattanon.hu@gmail.com>
2022-01-08 18:25:16 +0700

    serialize with user dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from '../user/dtos/user.dto';
 import { RegisterDto } from '../user/dtos/register.dto';
 import { AuthService } from './auth.service';
 
 @Controller()
+@Serialize(UserDto)
 export class AuthController {
   constructor(private authService: AuthService) {}
 
