6c83ab8643de48cfe1d93340b2780c06e63b96ea
KKCORR <nattanon.hu@gmail.com>
2022-01-08 18:53:01 +0700

    implement getToken

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,21 @@
 import * as bcrypt from 'bcrypt';
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { RegisterDto } from '../user/dtos/register.dto';
+import { UserDocument } from '../user/schema/user.schema';
 import { UserService } from '../user/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  getToken(user: UserDocument) {
+    const payload = { id: user._id, email: user.email, role: user.role };
+    return this.jwtService.sign(payload);
+  }
 
   async register(registerDto: RegisterDto) {
     const { email, password, role } = registerDto;
