88502dc99d3c57cdd3684d404f0f4669a7c43eed
KKCORR <nattanon.hu@gmail.com>
2022-01-08 18:51:55 +0700

    register JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: (config: ConfigService) => {
+        return {
+          secret: config.get<string>('jwt.secret'),
+          signOptions: {
+            expiresIn: config.get<string>('jwt.expiresIn'),
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
+    UserModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
