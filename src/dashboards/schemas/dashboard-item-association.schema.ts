aa2b4c88827989f3a041fca61f7b9e0a6f28cc02
techid2000 <techid2000@gmail.com>
2022-01-12 21:52:26 +0700

    Add DashboardItemAssociation schema

---
+++ src/dashboards/schemas/dashboard-item-association.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { DashboardItem } from './dashboard-item.schema';
+
+export type DashboardItemAssociationDocument = DashboardItemAssociation &
+  mongoose.Document;
+
+@Schema()
+export class DashboardItemAssociation {
+  _id: mongoose.Types.ObjectId;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: DashboardItem.name })
+  dashboardItem: DashboardItem;
+
+  @Prop()
+  x: number;
+
+  @Prop()
+  y: number;
+}
+
+export const DashboardItemAssociationSchema = SchemaFactory.createForClass(
+  DashboardItemAssociation,
+);
