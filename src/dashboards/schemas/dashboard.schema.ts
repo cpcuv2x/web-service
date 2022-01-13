98ac6f5990a607cde8586d1b06dd4f62769a98ee
techid2000 <techid2000@gmail.com>
2022-01-13 17:17:08 +0700

    Add User relationship for Dashboard

--- src/dashboards/schemas/dashboard.schema.ts
+++ src/dashboards/schemas/dashboard.schema.ts
@@ -1,12 +1,17 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { User } from 'src/user/schema/user.schema';
 import { DashboardItemAssociation } from './dashboard-item-association.schema';
+
 export type DashboardDocument = Dashboard & mongoose.Document;
 
 @Schema()
 export class Dashboard {
   @Prop()
   name: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: User.name, select: false })
+  user: User;
 
   @Prop([
     {
