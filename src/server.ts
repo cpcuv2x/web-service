a21d4c2c9fd3fa79597d7e90d1b3d420aea0fcb4
techid2000 <techid2000@gmail.com>
2022-02-12 12:13:18 +0700

    Add auth routes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
+import { authRouter } from "./auth/controllers";
 import { config } from "./commons/config";
 import { errorHandling } from "./commons/errorHandling";
 import { swaggerSpecs } from "./commons/swagger";
 import { dashboardsRouter } from "./dashboards/controllers";
 
 const app = express();
+
 app.use(express.json());
 
+app.use("/auth", authRouter);
 app.use("/dashboards", dashboardsRouter);
 
 app.use(errorHandling());
