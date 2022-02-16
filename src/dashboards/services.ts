7cdab8b65e4108529297eb19438e4b01cbe96482
techid2000 <techid2000@gmail.com>
2022-02-16 14:14:31 +0700

    Add relationship between Dashboard and User

--- src/dashboards/services.ts
+++ src/dashboards/services.ts
@@ -1,5 +1,4 @@
 import { Prisma, PrismaClient } from "@prisma/client";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import createHttpError from "http-errors";
 import {
   CreateDashboardDto,
@@ -9,134 +8,202 @@ import {
 
 const prisma = new PrismaClient();
 
-export const createDashboard = (payload: CreateDashboardDto) => {
+export const createDashboard = (
+  userId: string,
+  payload: CreateDashboardDto
+) => {
   return prisma.$transaction(async (prisma) => {
     if (payload.default) {
-      await uncheckDefaultDashboard();
+      await uncheckDefaultDashboard(userId);
     }
     const dashboard = await prisma.dashboard.create({
-      data: payload,
+      data: { userId, ...payload },
     });
     return dashboard;
   });
 };
 
-export const getAllDashboards = () => {
+export const getAllDashboards = (userId: string) => {
   return prisma.dashboard.findMany({
+    where: {
+      userId,
+    },
     orderBy: {
       name: Prisma.SortOrder.asc,
     },
   });
 };
 
-export const getDashboardById = async (id: string) => {
+export const getDashboardById = async (userId: string, dashboardId: string) => {
   const dashboard = await prisma.dashboard.findUnique({
     where: {
-      id,
+      id: dashboardId,
     },
     include: {
       components: true,
     },
   });
   if (!dashboard) {
-    throw new createHttpError.NotFound("Dashboard was not found.");
+    throw new createHttpError.NotFound(
+      `Dashboard "${dashboardId}" was not found.`
+    );
+  }
+  if (dashboard.userId !== userId) {
+    throw new createHttpError.Forbidden(
+      `Dashboard "${dashboardId}" cannot be accessed.`
+    );
   }
   return dashboard;
 };
 
-export const updateDashboard = (id: string, payload: UpdateDashboardDto) => {
+export const updateDashboard = (
+  userId: string,
+  dashboardId: string,
+  payload: UpdateDashboardDto
+) => {
   return prisma.$transaction(async (prisma) => {
+    const dashboard = await prisma.dashboard.findUnique({
+      where: {
+        id: dashboardId,
+      },
+    });
+    if (!dashboard) {
+      throw new createHttpError.NotFound(
+        `Dashboard "${dashboardId}" was not found.`
+      );
+    }
+    if (dashboard.userId !== userId) {
+      throw new createHttpError.Forbidden(
+        `Dashboard "${dashboardId}" cannot be accessed.`
+      );
+    }
     if (payload.default) {
-      await uncheckDefaultDashboard();
+      await uncheckDefaultDashboard(userId);
     }
-    try {
-      return await prisma.dashboard.update({
-        where: { id },
-        data: payload,
-      });
-    } catch (error) {
-      const prismaError = error as PrismaClientKnownRequestError;
-      if (prismaError.code === "P2025") {
-        throw new createHttpError.NotFound("Dashboard was not found.");
-      } else {
-        throw new createHttpError.InternalServerError(
-          "Update dashboard failed."
-        );
-      }
-    }
+    return prisma.dashboard.update({
+      where: { id: dashboardId },
+      data: payload,
+    });
   });
 };
 
-export const deleteDashboard = async (id: string) => {
-  try {
-    return await prisma.dashboard.delete({ where: { id } });
-  } catch (error) {
-    const prismaError = error as PrismaClientKnownRequestError;
-    if (prismaError.code === "P2025") {
-      throw new createHttpError.NotFound("Dashboard was not found.");
-    } else {
-      throw new createHttpError.InternalServerError("Delete dashboard failed.");
-    }
+export const deleteDashboard = async (userId: string, dashboardId: string) => {
+  const dashboard = await prisma.dashboard.findUnique({
+    where: {
+      id: dashboardId,
+    },
+  });
+  if (!dashboard) {
+    throw new createHttpError.NotFound(
+      `Dashboard "${dashboardId}" was not found.`
+    );
   }
+  if (dashboard.userId !== userId) {
+    throw new createHttpError.Forbidden(
+      `Dashboard "${dashboardId}" cannot be accessed.`
+    );
+  }
+  return prisma.dashboard.deleteMany({ where: { id: dashboardId, userId } });
 };
 
-const uncheckDefaultDashboard = () =>
+const uncheckDefaultDashboard = (userId: string) =>
   prisma.dashboard.updateMany({
-    where: { default: true },
+    where: { userId, default: true },
     data: { default: false },
   });
 
 export const updateDashboardComponents = (
-  id: string,
+  userId: string,
+  dashboardId: string,
   payload: UpdateDashboardComponentsDto
 ) => {
   return prisma.$transaction(async (prisma) => {
-    let created, updated, deleted;
-    try {
-      created = await prisma.dashboardComponentAssociation.createMany({
-        data: payload.create.map((creation) => ({
-          ...creation,
-          dashboardId: id,
-        })),
-      });
-    } catch (error) {
-      const prismaError = error as PrismaClientKnownRequestError;
-      if (prismaError.code === "P2003") {
-        throw new createHttpError.BadRequest(
-          "Update dashboard components failed on creation, some of the entities were not found."
-        );
-      } else {
-        throw new createHttpError.InternalServerError(
-          "Update dashboard components failed on creation."
-        );
-      }
-    }
-    try {
-      updated = await Promise.all(
-        payload.update.map((update) => {
-          const { dashboardComponentAssociationId, ...rest } = update;
-          return prisma.dashboardComponentAssociation.update({
-            where: { id: dashboardComponentAssociationId },
-            data: rest,
-          });
-        })
-      );
-    } catch (error) {
-      const prismaError = error as PrismaClientKnownRequestError;
-      if (prismaError.code === "P2025") {
-        throw new createHttpError.BadRequest(
-          "Update dashboard components failed on update, some of the entities were not found."
-        );
-      } else {
-        throw new createHttpError.InternalServerError(
-          "Update dashboard components failed on update."
-        );
-      }
-    }
-    deleted = await prisma.dashboardComponentAssociation.deleteMany({
-      where: { id: { in: payload.delete } },
+    const dashboard = await prisma.dashboard.findUnique({
+      where: {
+        id: dashboardId,
+      },
     });
-    return { created, updated: { count: updated.length }, deleted };
+    if (!dashboard) {
+      throw new createHttpError.NotFound(
+        `Dashboard "${dashboardId}" was not found.`
+      );
+    }
+    if (dashboard.userId !== userId) {
+      throw new createHttpError.Forbidden(
+        `Dashboard "${dashboardId}" cannot be accessed.`
+      );
+    }
+
+    let created, updated, deleted;
+
+    created = await Promise.all(
+      payload.create.map(async (creation) => {
+        const { dashboardComponentId, ...rest } = creation;
+        const dashboardComponent = await prisma.dashboardComponent.findUnique({
+          where: { id: dashboardComponentId },
+        });
+        if (!dashboardComponent) {
+          throw new createHttpError.NotFound(
+            `Dashboard component "${dashboardComponentId}" was not found.`
+          );
+        }
+        return prisma.dashboardComponentAssociation.create({
+          data: { ...creation, dashboardId },
+        });
+      })
+    );
+
+    updated = await Promise.all(
+      payload.update.map(async (update) => {
+        const { dashboardComponentAssociationId, ...rest } = update;
+        const dashboardComponentAssociation =
+          await prisma.dashboardComponentAssociation.findUnique({
+            where: { id: dashboardComponentAssociationId },
+          });
+        if (!dashboardComponentAssociation) {
+          throw new createHttpError.NotFound(
+            `Dashboard component association "${dashboardComponentAssociationId}" was not found.`
+          );
+        }
+        if (dashboardComponentAssociation.dashboardId !== dashboardId) {
+          throw new createHttpError.Forbidden(
+            `Dashboard component association "${dashboardComponentAssociationId}" cannot be accessed.`
+          );
+        }
+        return prisma.dashboardComponentAssociation.update({
+          where: { id: dashboardComponentAssociationId },
+          data: rest,
+        });
+      })
+    );
+
+    deleted = await Promise.all(
+      payload.delete.map(async (dashboardComponentAssociationId) => {
+        const dashboardComponentAssociation =
+          await prisma.dashboardComponentAssociation.findUnique({
+            where: { id: dashboardComponentAssociationId },
+          });
+        if (!dashboardComponentAssociation) {
+          throw new createHttpError.NotFound(
+            `Dashboard component association ${dashboardComponentAssociationId} was not found.`
+          );
+        }
+        if (dashboardComponentAssociation.dashboardId !== dashboardId) {
+          throw new createHttpError.Forbidden(
+            `Dashboard component association ${dashboardComponentAssociationId} cannot be accessed.`
+          );
+        }
+        return prisma.dashboardComponentAssociation.delete({
+          where: { id: dashboardComponentAssociationId },
+        });
+      })
+    );
+
+    return {
+      created: { count: created.length },
+      updated: { count: updated.length },
+      deleted: { count: deleted.length },
+    };
   });
 };
 
