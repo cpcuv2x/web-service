27c589d54f6c6d78d8ef680feff872c7b7fa5125
Thanadol <thanadol_2000@windowslive.com>
2022-03-04 18:05:05 +0700

    Add driver-related data structures and validation.

---
+++ src/routes/drivers/interfaces.ts
@@ -0,0 +1,55 @@
+import * as core from "express-serve-static-core";
+
+export interface CreateDriverDto {
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+  nationalId: string;
+  carDrivingLicenseId: string;
+}
+
+export interface CreateDriverModelDto {
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+  nationalId: string;
+  carDrivingLicenseId: string;
+  imageFilename: string;
+}
+
+export interface SearchDriversCriteriaQuery extends core.Query {
+  firstName?: string;
+  lastName?: string;
+  nationalId?: string;
+  carDrivingLicenseId?: string;
+  imageFilename?: string;
+  startBirthDate?: string;
+  endBirthDate?: string;
+  limit?: string;
+  offset?: string;
+  orderBy?: string;
+  orderDir?: string;
+}
+
+export interface SearchDriversCriteria {
+  firstName: string | undefined;
+  lastName: string | undefined;
+  nationalId: string | undefined;
+  carDrivingLicenseId: string | undefined;
+  imageFilename: string | undefined;
+  startBirthDate: string | undefined;
+  endBirthDate: string | undefined;
+  limit: number;
+  offset: number;
+  orderBy: string;
+  orderDir: "asc" | "desc";
+}
+
+export interface UpdateDriverDto {
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+  nationalId: string;
+  carDrivingLicenseId: string;
+  imageFilename: string;
+}
