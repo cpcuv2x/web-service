671005db912f0fbef185a3d7ab79547d9002b35b
techid2000 <techid2000@gmail.com>
2022-03-25 21:06:56 +0700

    Added interface EventMessageRaw and EventMessage

---
+++ src/components/kafka-consumer/interfaces.ts
@@ -0,0 +1,27 @@
+import { EventMessageType, EventStatus } from "./enums";
+
+export interface EventMessageRaw {
+  car_id?: string;
+  driver_id?: string;
+  camera_id?: string;
+  lat?: string;
+  lng?: string;
+  time?: number;
+  type?: string;
+  passenger?: number;
+  ecr?: number;
+  status?: string;
+}
+
+export interface EventMessage {
+  type?: EventMessageType;
+  time?: number;
+  carId?: string;
+  driverId?: string;
+  cameraId?: string;
+  lat?: number;
+  lng?: number;
+  passengers?: number;
+  ecr?: number;
+  status?: EventStatus;
+}
